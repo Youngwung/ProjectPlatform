1c4a64bca454b6f69db33f2103bdc57513b1dd5a
Youngwung <chenld0728@gmail.com>
2025-01-18 17:50:02 +0900

    Update root.jsx
    코드 스플리팅 적용

--- frontend/src/router/root.jsx
+++ frontend/src/router/root.jsx
@@ -1,58 +1,48 @@
 import React, { lazy, Suspense } from "react";
 import { createBrowserRouter as Router } from "react-router-dom";
+import joinProjectRouter from "./joinProjectRouter";
 
 const Loading = <div>Loading....</div>; // 로딩 중에 보여줄 요소
 const Layout = lazy(() => import("../Layout/Layout"));
 const ErrorPage = lazy(() => import("../pages/ErrorPage"));
 const Main = lazy(() => import("../pages/Main"));
 const FindProject = lazy(() => import("../pages/FindProject"));
-const JoinProject = lazy(() => import("../pages/JoinProject"));
+const JoinProjectIndex = lazy(() => import("../pages/joinProject/IndexPage"));
 const MyPage = lazy(() => import("../pages/MyPage"));
 
 const root = Router([
 	{
 		path: "/",
 		element: (
-			<Suspense fallback={Loading}>
-				<Layout />
-			</Suspense>
+			<Suspense fallback={Loading}><Layout /></Suspense>
 		),
 		errorElement: (
-			<Suspense fallback={Loading}>
-				<ErrorPage />
-			</Suspense>
+			<Suspense fallback={Loading}><ErrorPage /></Suspense>
 		), // 에러 페이지 처리
 		children: [
 			{
 				path: "/",
 				element: (
-					<Suspense fallback={Loading}>
-						<Main />
-					</Suspense>
+					<Suspense fallback={Loading}><Main /></Suspense>
 				),
 			},
 			{
 				path: "/findProject",
 				element: (
-					<Suspense fallback={Loading}>
-						<FindProject />
-					</Suspense>
+					<Suspense fallback={Loading}><FindProject /></Suspense>
 				),
 			},
 			{
 				path: "/joinProject",
 				element: (
-					<Suspense fallback={Loading}>
-						<JoinProject />
-					</Suspense>
+					<Suspense fallback={Loading}><JoinProjectIndex /></Suspense>
 				),
+				children: joinProjectRouter(),
 			},
 			{
 				path: "/mypage",
 				element: (
-					<Suspense fallback={Loading}>
-						<MyPage />
-					</Suspense>
+					<Suspense fallback={Loading}><MyPage /></Suspense>
 				),
 			},
 		],

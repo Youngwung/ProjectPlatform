7356bf17e8724b207644728a498c41e2e250628a
GreatOvOb <dlwjddls888@gmail.com>
2025-01-24 15:29:43 +0900

    feat : linktypeRepo

--- backend/src/main/java/com/ppp/backend/repository/LinkTypeRepository.java
+++ backend/src/main/java/com/ppp/backend/repository/LinkTypeRepository.java
@@ -1,8 +1,10 @@
 package com.ppp.backend.repository;
 
+import com.ppp.backend.domain.LinkType;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LinkTypeRepository {
+public interface LinkTypeRepository extends JpaRepository<LinkType, Long> {
 
 }

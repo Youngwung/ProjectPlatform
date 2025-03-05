f9c283501ba7466071a6e6d407ebe93b5d797c10
Youngwung <chenld0728@gmail.com>
2025-03-05 16:34:17 +0900

    Update create_table.sql
    프로젝트 참여 정보 테이블, 신청 횟수, 초대 횟수 테이블 생성

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -148,3 +148,21 @@ CREATE TABLE `alert_project` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE project_members (
+	id bigint NOT NULL AUTO_INCREMENT,
+	project_id bigint NOT NULL,
+	user_id bigint NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE project_applications (
+	project_id bigint NOT NULL,
+	counts bigint NOT NULL,
+	PRIMARY KEY (`project_id`)
+);
+
+CREATE TABLE portfolio_invitations (
+	portfolio_id bigint NOT NULL,
+	counts bigint NOT NULL,
+	PRIMARY KEY (`portfolio_id`)
+);

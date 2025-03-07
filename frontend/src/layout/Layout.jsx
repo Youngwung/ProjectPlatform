744400803e153a10bab777bbd4dff2330754d791
Youngwung <chenld0728@gmail.com>
2025-03-07 17:57:23 +0900

    update layout

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Container} from 'react-bootstrap'
-import Top from './Top'
-import Footer from './Footer'
+import { Container } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+import Top from './Top'
 
 const Layout = () => {
   return (
